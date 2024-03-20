fec83d2beafd0baf125ea1d64bef45a73826d3fd
MonashTee <akinmolayanseun@gmail.com>
2024-03-20 10:49:06 +0100

    commit on while loops

---
+++ while.py
@@ -0,0 +1,20 @@
+#Example 1
+counter = 0
+while counter <= 4:
+    print ("I love learning python")
+    counter = counter + 1
+
+#Example 2
+# Initialize a variable
+count = 0
+
+# Define the condition for the while loop
+while count < 5:
+    # Print the current value of count
+    print("Count:", count)
+    
+    # Increment the count by 2
+    count += 2
+
+print("While loop finished.")
+
