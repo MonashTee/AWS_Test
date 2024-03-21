8781d44ebb46f0fc9a00a2743504b0a242147747
MonashTee <akinmolayanseun@gmail.com>
2024-03-21 04:49:54 +0100

    Commit on dictionary using Python.

---
+++ Dictionary.py
@@ -0,0 +1,34 @@
+#Example 1
+the_dict = {"brand": "suv",
+          "year": 2023,
+          "style": "classic",
+          "color": "black"}
+print (the_dict)
+print (the_dict["style"])
+print (the_dict["color"])
+
+#Example 2
+myDict = {}
+myDict["one"] = 1
+myDict["two"] = 2
+myDict[3] = "three"
+myDict["four"] = 4.4
+print (myDict[3])
+print (myDict)
+
+#Example 3
+#using the dict() constructor to make  a dictionary.
+thisdict = dict(name = "Tehillah", age = 2, country = "Germany")
+print (thisdict)
+
+#Example 4
+own_dict = {"Education": "formal",
+            "Profession": "Lecturer",
+            "University": "Paderborn",
+            "Hobby": "Travelling",
+            "Status": "Married",
+            "Age": 35
+            }
+print (own_dict)
+print (own_dict["Status"])
+
