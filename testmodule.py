aaf215447a2dc87d86d0b2d59257ab9ac5be13f7
MonashTee <akinmolayanseun@gmail.com>
2024-03-23 12:56:35 +0100

    importing code from own module

---
+++ testmodule.py
@@ -0,0 +1,15 @@
+from mymodule import calculate_circle_area
+print (calculate_circle_area(5))
+
+import mymodule
+a = mymodule.Student1["name"]
+print(a)
+
+from mymodule import Dept
+print(Dept[3])
+
+import mymodule
+print (mymodule.Car["Brand"])
+
+from mymodule import Patient
+print (Patient)
