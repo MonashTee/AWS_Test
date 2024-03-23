959ae748f4dd5f87277ee935daf59be75f577a70
MonashTee <akinmolayanseun@gmail.com>
2024-03-23 12:20:31 +0100

    creating own module in python

---
+++ mymodule.py
@@ -0,0 +1,26 @@
+##Example 1
+import math
+def calculate_circle_area(radius):
+    return math.pi * radius**2
+
+##Example 2
+Student1 = {"name": "David",
+            "Age": 24,
+            "Country": "Norway",
+            "Department": "Science",
+            "Course": "IMS"
+            }
+
+##Example 3
+Dept = ["Choir", "Prayer", "Ushering", "Welfare", "Followup"]
+
+##Example 4
+Car = {"Brand": "Toyota",
+       "Model": "Big boy",
+       "Color": "Grey",
+       "Year": 2024,
+       "Design": "Stylish",
+       }
+
+##Example 5
+Patient = dict(name = "Stella", age = 75, country = "Spain", diagnosis = "Fever")
